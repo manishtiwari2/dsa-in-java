6229b9c6206b09db87c43e6ac4303de05aa6ad81
Manish Tiwari <manishtiwari2578@gmail.com>
2025-12-28 09:59:37 +0530

    deleted file

---
+++ src/basics/maths/CountDigit.java
@@ -0,0 +1,22 @@
+package basics.maths;
+
+public class CountDigit {
+
+    public int countDigits(int n) {
+        if (n == 0) return 1;
+
+        n = Math.abs(n);
+        int count = 0;
+
+        while (n > 0) {
+            count++;
+            n /= 10;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        CountDigit digit = new CountDigit();
+        System.out.println(digit.countDigits(-7672)); // 4
+    }
+}
