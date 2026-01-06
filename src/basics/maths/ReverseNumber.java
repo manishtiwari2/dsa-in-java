f7fd62c79f824a14f589e8ab8c59892a3d1bb413
Manish Tiwari <manishtiwari2578@gmail.com>
2026-01-06 15:57:40 +0530

    added some questions code

---
+++ src/basics/maths/ReverseNumber.java
@@ -0,0 +1,21 @@
+package basics.maths;
+
+public class ReverseNumber {
+    public int reverseNumber(int num){
+        int revNum = 0;
+        while(num>0){
+           int last = num%10;
+           revNum = revNum*10 + last;
+           num = num/10;
+        }
+        return revNum;
+    }
+
+    public static void main(String[] args) {
+        ReverseNumber revNum = new ReverseNumber();
+        int ans = revNum.reverseNumber(123);
+        System.out.println(ans);
+    }
+}
+// 0
+// 16=  6 === rever*10 + 6 == 10
