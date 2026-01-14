958a237f2575d5c3b0229e72dc5eedaf34f310e7
Manish Tiwari <manishtiwari2578@gmail.com>
2026-01-14 17:41:21 +0530

    added all files

---
+++ src/basics/recursion/SumN.java
@@ -0,0 +1,18 @@
+package basics.recursion;
+
+public class SumN {
+    public int sumN(int n){
+        if(n == 0){
+            return 0;
+        }
+        return n + sumN(n-1);
+    }
+
+    public static void main(String[] args) {
+        SumN sm = new SumN();
+        System.out.println(sm.sumN(5));
+    }
+}
+
+
+
