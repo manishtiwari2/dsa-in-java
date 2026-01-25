7036377eede1d60b8b50ed0699291787ada3aaeb
Manish Tiwari <manishtiwari2578@gmail.com>
2026-01-25 16:51:45 +0530

    added mojaroty element code

--- src/Arrays/medium/MajorityElement.java
+++ src/Arrays/medium/MajorityElement.java
@@ -1,29 +1,50 @@
 package Arrays.medium;
+import java.util.HashMap;
 
 public class MajorityElement {
+    public int majorityElement(int[] nums){
+        int check = 0;
+        int majNum = 0;
+
+        for (int num : nums) {
+            if (check == 0) {
+                majNum = num;
+            }
+            if (num == majNum) {
+                check++;
+            } else {
+                check--;
+            }
+        }
+        return majNum;
+    }
+    public static void main(String[] args) {
+        MajorityElement obj = new MajorityElement();
+        int[] nums = {2, 2, 1, 1, 1, 2,1};
+        int result = obj.majorityElement(nums);
+        System.out.println("Majority Element: " + result);
+    }
 }
 
-//
-//public int majorityElement(int[] nums) {
-//    int max = nums.length/2;
-//    HashMap<Integer,Integer> map = new HashMap<>();
-//    for(int num: nums){
-//        if(map.containsKey(num)){
-//            map.put(num,map.get(num) + 1);
-//        } else {
-//            map.put(num, 1);
-//        }
-//    }
 
+// HashMap - frequency count algorithm
+
+//public int majorityElement(int[] nums) {
+//    int max = nums.length / 2;
+//    HashMap<Integer, Integer> map = new HashMap<>();
+//    // Count frequency
+//    for (int num : nums) {
+//        map.put(num, map.getOrDefault(num, 0) + 1);
+//    }
 //    int maxNum = 0;
 //    int freq = -1;
-//    for(int key: map.keySet()){
-//        if(map.get(key) > freq){
+//    // Find element with highest frequency
+//    for (int key : map.keySet()) {
+//        if (map.get(key) > freq) {
 //            freq = map.get(key);
 //            maxNum = key;
 //        }
-//    }
-//    if(freq > max){
+//    } if (freq > max) {
 //        return maxNum;
 //    }
 //    return -1;
