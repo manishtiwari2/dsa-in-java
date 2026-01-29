7219e9ce422b913513cecd4dcdd8408a4c17f45c
Manish Tiwari <manishtiwari2578@gmail.com>
2026-01-29 15:10:56 +0530

    added all files

---
+++ src/Arrays/medium/LongestConsecutiveSequence.java
@@ -0,0 +1,38 @@
+package Arrays.medium;
+
+import java.util.HashSet;
+
+public class LongestConsecutiveSequence {
+
+    public int longestConsecutive(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        int maxCount = 0;
+        for (int num : set) {
+            if (!set.contains(num - 1)) {
+                int currentNum = num;
+                int count = 1;
+                while (set.contains(currentNum + 1)) {
+                    currentNum++;
+                    count++;
+                }
+                maxCount = Math.max(maxCount, count);
+            }
+        }
+        return maxCount;
+    }
+    public static void main(String[] args) {
+        LongestConsecutiveSequence obj = new LongestConsecutiveSequence();
+
+        int[] nums1 = {100, 4, 200, 1, 3, 2};
+        int[] nums2 = {0, 3, 7, 2, 5, 8, 4, 6, 0, 1};
+        int[] nums3 = {};
+
+        System.out.println(obj.longestConsecutive(nums1));
+        System.out.println(obj.longestConsecutive(nums2));
+        System.out.println(obj.longestConsecutive(nums3));
+    }
+}
+
--- src/Strings/medium/EncodeDecode.java
+++ src/Strings/medium/EncodeDecode.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class EncodeDecode {
     public String encode(List<String> strs) {
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < strs.size(); i++){
+        for (int i = 0; i < strs.size(); i++) {
             String curr = strs.get(i);
             int length = curr.length();
             sb.append(length);
@@ -15,6 +15,7 @@ public class EncodeDecode {
         }
         return sb.toString();
     }
+
     public List<String> decode(String str) {
         List<String> arr = new ArrayList<>();
         int i = 0;
@@ -24,7 +25,6 @@ public class EncodeDecode {
                 len = len * 10 + (str.charAt(i) - '0');
                 i++;
             }
-
             i++;
             StringBuilder sb = new StringBuilder();
             while (len > 0) {
@@ -37,4 +37,24 @@ public class EncodeDecode {
         return arr;
     }
 
+    public static void main(String[] args) {
+        EncodeDecode codec = new EncodeDecode();
+
+        List<String> input = new ArrayList<>();
+        input.add("hello");
+        input.add("world");
+        input.add("123*abc");
+        input.add("");
+
+        System.out.println("Original List:");
+        System.out.println(input);
+
+        String encoded = codec.encode(input);
+        System.out.println("\nEncoded String:");
+        System.out.println(encoded);
+
+        List<String> decoded = codec.decode(encoded);
+        System.out.println("\nDecoded List:");
+        System.out.println(decoded);
+    }
 }
